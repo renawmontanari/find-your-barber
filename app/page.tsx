05761c7756f11240a1df3f7fff08e6e53c683d5b
renawmontanari <regiangelica@gmail.com>
2024-11-07 14:44:05 -0300

    feat: criado o input de pesquisa

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import { SearchIcon } from "lucide-react"
 import { Header } from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
   return (
     <div>
       {/* Header */}
       <Header />
+      <div className="p-5">
+        <h2 className="text-xl">Olá, Renan!</h2>
+        <p>Quinta-feira, 7 de novembro.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Encontre seu barbeiro..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
