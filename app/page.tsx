155b42f119dbcf140c846b0e12f1346b6e474e4a
renawmontanari <regiangelica@gmail.com>
2024-11-08 12:09:36 -0300

    feat: adicioando o book dos itens

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Header } from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -13,6 +16,7 @@ export default function Home() {
         <h2 className="text-xl">Olá, Renan!</h2>
         <p>Quinta-feira, 7 de novembro.</p>
 
+        {/* Search */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Encontre seu barbeiro..." />
           <Button>
@@ -20,6 +24,7 @@ export default function Home() {
           </Button>
         </div>
 
+        {/* Image */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/assets/banner-01.png"
@@ -28,6 +33,30 @@ export default function Home() {
             alt="Agende nos melhores com NSF Barber"
           />
         </div>
+
+        {/* Scheduling */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://thumbs.dreamstime.com/b/imagem-redonda-do-avatar-dos-desenhos-animados-ilustra-o-da-arte-vetor-143071694.jpg" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Novembro</p>
+              <p className="text-2xl">08</p>
+              <p className="text-sm">13:30</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
