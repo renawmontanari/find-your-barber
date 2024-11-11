fb7579fbad12dead83b74649828282252a5f5e28
renawmontanari <regiangelica@gmail.com>
2024-11-11 11:34:56 -0300

    feat: configurando para receber imagem do servidor

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -1,3 +1,20 @@
-export function BarbershopItem({ barbershop }) {
-  return <h1>{barbershop.name}</h1>
+import { Barbershop } from "@prisma/client"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+
+interface BarbershopItemProps {
+  barbershop: Barbershop
+}
+
+export function BarbershopItem({ barbershop }: BarbershopItemProps) {
+  return (
+    <Card>
+      <CardContent>
+        {/* Imagem */}
+        <div className="relative h-[159px]">
+          <Image fill className="object-contain" src={barbershop.imageUrl} />
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
