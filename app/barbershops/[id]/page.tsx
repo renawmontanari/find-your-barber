bf0d53875ff63c8a86727657ac4ef94a30ace2b6
renawmontanari <regiangelica@gmail.com>
2024-11-15 13:44:16 -0300

    feat: tela inicial do barbershop

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 interface BarbershopPageProps {
   params: {
@@ -17,6 +18,10 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       id: params.id,
     },
   })
+
+  if (!barbershop) {
+    return notFound()
+  }
 
   return (
     <div>
@@ -51,12 +56,12 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
 
       <div className="border-b border-solid p-5">
         <h1 className="mb-6 text-xl font-bold">{barbershop?.name}</h1>
-        <div className="mb-2 flex items-center gap-1">
+        <div className="mb-2 flex items-center gap-2">
           <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop?.address}</p>
         </div>
 
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-2">
           <StarIcon className="fill-primary text-primary" size={18} />
           <p className="text-sm">5,0 (499 avaliações) teste</p>
         </div>
