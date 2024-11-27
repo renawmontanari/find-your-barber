2fd3e060e62592022b2d5574f017287315144487
renawmontanari <regiangelica@gmail.com>
2024-11-27 20:10:07 -0300

    feat: utilizando hookform do shadcn e zod para validar

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -3,27 +3,54 @@
 import { SearchIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
-import { useState } from "react"
 import { useRouter } from "next/navigation"
 
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
+
+const formSchema = z.object({
+  search: z.string().trim().min(1, {
+    message: "O campo 'Pesquisar' é obrigatório",
+  }),
+  // Exemplo de outros inputs
+  // name: z.string().trim().min(1).max(255)
+})
+
 export default function Search() {
-  const [search, setSearch] = useState("")
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: "",
+    },
+  })
+
   const router = useRouter()
 
-  const handleSubmit = () => {
-    router.push(`/barbershops?search=${search}`)
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?search=${data.search}`)
   }
 
   return (
-    <div className="flex items-center gap-2">
-      <Input
-        placeholder="Encontre seu barbeiro..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <Button onClick={handleSubmit}>
-        <SearchIcon />
-      </Button>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
+        <FormField
+          control={form.control}
+          name="search"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <Input placeholder="Encontre seu barbeiro..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          <SearchIcon />
+        </Button>
+      </form>
+    </Form>
   )
 }
