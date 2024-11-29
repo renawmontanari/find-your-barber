a534d13ba13346fdb34be78c56060d9f4307d983
renawmontanari <regiangelica@gmail.com>
2024-11-29 20:04:28 -0300

    feat: construindo o menu de reserva

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -1,13 +1,59 @@
+"use client"
+
 import { BarbershopService } from "@prisma/client"
 import Image from "next/image"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
+import { Calendar } from "./ui/calendar"
+import { ptBR } from "date-fns/locale"
+import { useState } from "react"
 
 interface ServiceItemProps {
   service: BarbershopService
 }
 
+const TIME_LIST = [
+  "08:00",
+  "08:30",
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+  "19:30",
+  "20:00",
+]
+
 export default function ServiceItem({ service }: ServiceItemProps) {
+  const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined)
+
+  const handleDateSelect = (date: Date | undefined) => {
+    setSelectedDay(date)
+  }
+
   return (
     <Card>
       <CardContent className="item-center flex gap-3 p-3">
@@ -30,9 +76,64 @@ export default function ServiceItem({ service }: ServiceItemProps) {
               }).format(Number(service.price))}
             </p>
 
-            <Button variant="secondary" size="sm">
-              Reservar
-            </Button>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="secondary" size="sm">
+                  Reservar
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="px-0">
+                <SheetHeader>
+                  <SheetTitle>Fazer reserva</SheetTitle>
+                </SheetHeader>
+
+                <div className="border-b border-solid py-5">
+                  <Calendar
+                    mode="single"
+                    locale={ptBR}
+                    selected={selectedDay}
+                    onSelect={handleDateSelect}
+                    styles={{
+                      head_cell: {
+                        width: "100%",
+                        textTransform: "capitalize",
+                      },
+                      cell: {
+                        width: "100%",
+                      },
+                      button: {
+                        width: "100%",
+                      },
+                      nav_button_previous: {
+                        width: "32px",
+                        height: "32px",
+                      },
+                      nav_button_next: {
+                        width: "32px",
+                        height: "32px",
+                      },
+                      caption: {
+                        textTransform: "capitalize",
+                      },
+                    }}
+                  />
+                </div>
+
+                {selectedDay && (
+                  <div className="flex gap-3 overflow-x-auto p-5 [&::-webkit-scrollbar]:hidden">
+                    {TIME_LIST.map((time) => (
+                      <Button
+                        key={time}
+                        variant="outline"
+                        className="rounded-full"
+                      >
+                        {time}
+                      </Button>
+                    ))}
+                  </div>
+                )}
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </CardContent>
