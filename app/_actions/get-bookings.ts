55cd458613e71cbadba9c7cab3ce04fc3373dcf1
renawmontanari <regiangelica@gmail.com>
2024-12-05 18:53:57 -0300

    feat: adicionado uma actions para pegar os agendamentos

---
+++ app/_actions/get-bookings.ts
@@ -0,0 +1,18 @@
+import { endOfDay, startOfDay } from "date-fns"
+import { db } from "../_lib/prisma"
+
+interface GetBookingsProps {
+  serviceId: string
+  date: Date
+}
+
+export default function getBookings({ date }: GetBookingsProps) {
+  return db.booking.findMany({
+    where: {
+      date: {
+        lte: endOfDay(date),
+        gte: startOfDay(date),
+      },
+    },
+  })
+}
