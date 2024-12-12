14e6299e05834bd2276f3cce649121ca8633f73e
renawmontanari <regiangelica@gmail.com>
2024-12-12 20:37:39 -0300

    feat: desenvolvendo a tela de detalhes da reserva

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <Card>
         <CardContent className="px-5 py-6">
           <p className="text-xs text-gray-400">
-            © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            © 2024 Copyright <span className="font-bold">FSW Barber</span>
           </p>
         </CardContent>
       </Card>
